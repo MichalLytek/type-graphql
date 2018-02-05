7625b6245faa32858bd9bec094455031c913ec64
OJ <osorio.rodrigues@adesso.at>
2018-02-05 20:36:21 +0100

    https://github.com/19majkel94/type-graphql/issues/22 Changing default date format to isoString

--- src/types/date.scalar.ts
+++ src/types/date.scalar.ts
@@ -1,47 +1,20 @@
-import {Kind} from "graphql/language";
-import {GraphQLScalarType} from "graphql";
+import {GraphQLScalarType, Kind} from "graphql";
 
-function serializeDate(value: Date) {
-    if (value instanceof Date) {
-        return value.getTime();
-    } else if (typeof value === "number") {
-        return Math.trunc(value);
-    } else if (typeof value === "string") {
-        return Date.parse(value);
-    }
-    return null;
-}
-
-function parseDate(value: string) {
-    if (value === null) {
+export const GraphQLTimestampType = new GraphQLScalarType({
+    name: "Date",
+    description: "Date scalar type",
+    parseValue(value: string) {
+        return new Date(value); // value from the client input variables
+    },
+    serialize(value: Date) {
+        return value.toISOString(); // value sent to the client
+    },
+    parseLiteral(ast) {
+        if (ast.kind === Kind.STRING) {
+            return new Date(ast.value); // value from the client query
+        }
         return null;
-    }
-
-    try {
-        return new Date(value);
-    } catch (err) {
-        return null;
-    }
-}
-
-function parseDateFromLiteral(ast: any) {
-    if (ast.kind === Kind.INT) {
-        const num = parseInt(ast.value, 10);
-        return new Date(num);
-    } else if (ast.kind === Kind.STRING) {
-        return parseDate(ast.value);
-    }
-    return null;
-}
-
-const GraphQLTimestampType = new GraphQLScalarType({
-    name: "Timestamp",
-    description:
-    "The javascript `Date` as integer. Type represents date and time " +
-    "as number of milliseconds from start of UNIX epoch.",
-    serialize: serializeDate,
-    parseValue: parseDate,
-    parseLiteral: parseDateFromLiteral,
+    },
 });
 
 export default GraphQLTimestampType;
