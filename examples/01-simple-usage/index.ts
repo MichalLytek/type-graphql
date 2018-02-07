6ff2a5792e39e4e231b29f927bc222eef9a0a239
OJ <osorio.rodrigues@adesso.at>
2018-02-07 17:28:38 +0100

    chore(example/01/index.ts): Enabling direct click on URL from terminal

--- examples/01-simple-usage/index.ts
+++ examples/01-simple-usage/index.ts
@@ -20,5 +20,5 @@ app.use(
   }),
 );
 app.listen(4000, () => {
-  console.log("Running a GraphQL API server at localhost:4000/graphql");
+  console.log("Running a GraphQL API server at http://localhost:4000/graphql");
 });
