abab31d050e8edd47e565a6fc95ac3e7a5a1e3dc
Michał Lytek <michal.ly@wp.pl>
2018-02-21 20:41:57 +0100

    chore(test): remove useless pseudo test files

--- dev.js
+++ dev.js
@@ -1,6 +1,5 @@
 require("ts-node/register/type-check");
-require("./tests/experiments/index.ts");
-// require("./examples/01-simple-usage/index.ts");
+require("./examples/01-simple-usage/index.ts");
 // require("./examples/02-using-container/index.ts");
 // require("./examples/03-typeorm-usage/index.ts");
 // require("./examples/04-automatic-validation/index.ts");
