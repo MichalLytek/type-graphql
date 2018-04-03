3f339f5b90296cccaa52bc7875b0448ec57bac8c
Michał Lytek <michal.ly@wp.pl>
2018-04-03 20:23:48 +0200

    docs(webiste): configure website footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -5,17 +5,17 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const React = require('react');
+const React = require("react");
 
 class Footer extends React.Component {
   docUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
-    return baseUrl + 'docs/' + (language ? language + '/' : '') + doc;
+    return baseUrl + "docs/" + (language ? language + "/" : "") + doc;
   }
 
   pageUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
-    return baseUrl + (language ? language + '/' : '') + doc;
+    return baseUrl + (language ? language + "/" : "") + doc;
   }
 
   render() {
@@ -35,35 +35,38 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
-            </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              Guides (or other categories)
-            </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              API Reference (or other categories)
-            </a>
+            <a href={this.docUrl("introduction.html", this.props.language)}>Introduction</a>
+            <a href={this.docUrl("getting-started.html", this.props.language)}>Getting Started</a>
+            <a href={this.docUrl("scalars.html", this.props.language)}>Advanced Guides</a>
+            {/* <a href={this.docUrl('api.html', this.props.language)}>
+              API Reference
+            </a> */}
           </div>
           <div>
             <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
+            {/* <a href={this.pageUrl('users.html', this.props.language)}>
               User Showcase
-            </a>
-            <a
-              href="http://stackoverflow.com/questions/tagged/"
+            </a> */}
+            {/* <a
+              href="http://stackoverflow.com/questions/tagged/type-graphql"
               target="_blank">
               Stack Overflow
+            </a> */}
+            <a href={this.props.config.repoUrl + "/issues?utf8=✓&q=is%3Aissue+label%3A&quot;Enhancement+%3Anew%3A&quot;+"}>
+              Feature requests and proposals
             </a>
-            <a href="https://discordapp.com/">Project Chat</a>
-            <a href="https://twitter.com/" target="_blank">
+            <a href={this.props.config.repoUrl + "/issues?q=is%3Aissue+label%3A&quot;Bug+%3Abug%3A&quot;"}>
+              Issues
+            </a>
+            <a href="https://gitter.im/type-graphql/Lobby">Project Chat</a>
+            <a href="https://twitter.com/19majkel94" target="_blank">
               Twitter
             </a>
           </div>
           <div>
             <h5>More</h5>
-            <a href={this.props.config.baseUrl + 'blog'}>Blog</a>
-            <a href="https://github.com/">GitHub</a>
+            <a href={this.props.config.baseUrl + "blog"}>Blog</a>
+            <a href={this.props.config.repoUrl}>GitHub</a>
             <a
               className="github-button"
               href={this.props.config.repoUrl}
@@ -71,26 +74,13 @@ class Footer extends React.Component {
               data-count-href="/facebook/docusaurus/stargazers"
               data-show-count={true}
               data-count-aria-label="# stargazers on GitHub"
-              aria-label="Star this project on GitHub">
+              aria-label="Star this project on GitHub"
+            >
               Star
             </a>
           </div>
         </section>
-
-        <a
-          href="https://code.facebook.com/projects/"
-          target="_blank"
-          className="fbOpenSource">
-          <img
-            src={this.props.config.baseUrl + 'img/oss_logo.png'}
-            alt="Facebook Open Source"
-            width="170"
-            height="45"
-          />
-        </a>
-        <section className="copyright">
-          {this.props.config.copyright}
-        </section>
+        <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
   }
