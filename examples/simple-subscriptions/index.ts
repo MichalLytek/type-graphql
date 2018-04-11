02e07442606e0df8763bcd4e141c62c3595b50d4
Michał Lytek <michal.ly@wp.pl>
2018-04-11 16:30:36 +0200

    docs(examples): add graphql-yoga server to simple subscriptions

--- examples/simple-subscriptions/index.ts
+++ examples/simple-subscriptions/index.ts
@@ -1,52 +1,31 @@
 import "reflect-metadata";
-import * as http from "http";
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import { SubscriptionServer } from "subscriptions-transport-ws";
-import { execute, subscribe } from "graphql";
-import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
+import { GraphQLServer, Options } from "graphql-yoga";
 import { buildSchema } from "../../src";
 
 import { SampleResolver } from "./resolver";
 
 async function bootstrap() {
-  const HTTP_PORT = 4000;
-  const WS_PORT = 4001;
-
   // Build the TypeGraphQL schema
   const schema = await buildSchema({
     resolvers: [SampleResolver],
   });
 
-  // Create WebSocket listener server
-  const websocketServer = http.createServer((request, response) => {
-    response.writeHead(404);
-    response.end();
-  });
+  // Create GraphQL server
+  const server = new GraphQLServer({ schema });
 
-  // Bind it to port and start listening
-  websocketServer.listen(WS_PORT, () =>
-    console.log(`Websocket Server is now running on localhost:${WS_PORT}`),
-  );
+  // Configure server options
+  const serverOptions: Options = {
+    port: 4000,
+    endpoint: "/graphql",
+    subscriptions: "/graphql",
+    playground: "/playground",
+  };
 
-  // Create Subscription Server to handle subscriptions over WS
-  const subscriptionServer = SubscriptionServer.create(
-    { schema, execute, subscribe },
-    { server: websocketServer, path: "/graphql" },
-  );
-
-  // create express-based gql endpoint
-  const app = express();
-  app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
-  app.use(
-    "/graphiql",
-    graphiqlExpress({
-      endpointURL: "/graphql",
-      subscriptionsEndpoint: `ws://localhost:${WS_PORT}/graphql`,
-    }),
-  );
-  app.listen(HTTP_PORT, () => {
-    console.log(`Running a GraphQL API server at localhost:${HTTP_PORT}/graphql`);
+  // Start the server
+  server.start(serverOptions, ({ port, playground }) => {
+    console.log(
+      `Server is running, GraphQL Playground available at http://localhost:${port}${playground}`,
+    );
   });
 }
 
