4d160691826d3bc251764704e2a66e8b8c4ada07
Michał Lytek <michal.ly@wp.pl>
2018-04-13 12:32:16 +0200

    feat(middleware): create middleware stack in resolvers handlers

--- src/resolvers/create.ts
+++ src/resolvers/create.ts
@@ -9,7 +9,7 @@ import {
 import { getParams, checkForAccess } from "./helpers";
 import { convertToType } from "../helpers/types";
 import { BuildContext } from "../schema/build-context";
-import { ActionData } from "../types";
+import { ActionData, AuthChecker } from "../types";
 
 export function createHandlerResolver(
   resolverMetadata: BaseResolverMetadata,
@@ -19,14 +19,15 @@ export function createHandlerResolver(
 
   return async (root, args, context, info) => {
     const actionData: ActionData = { root, args, context, info };
-    await checkForAccess(actionData, authChecker, resolverMetadata.roles);
-    const params: any[] = await getParams(
-      resolverMetadata.params!,
-      actionData,
-      globalValidate,
-      pubSub,
-    );
-    return resolverMetadata.handler!.apply(targetInstance, params);
+    return applyMiddlewares(resolverMetadata, actionData, authChecker, async () => {
+      const params: any[] = await getParams(
+        resolverMetadata.params!,
+        actionData,
+        globalValidate,
+        pubSub,
+      );
+      return resolverMetadata.handler!.apply(targetInstance, params);
+    });
   };
 }
 
@@ -42,20 +43,21 @@ export function createAdvancedFieldResolver(
 
   return async (root, args, context, info) => {
     const actionData: ActionData = { root, args, context, info };
-    await checkForAccess(actionData, authChecker, fieldResolverMetadata.roles);
     const targetInstance: any = convertToType(targetType, root);
-    // method
-    if (fieldResolverMetadata.handler) {
-      const params: any[] = await getParams(
-        fieldResolverMetadata.params!,
-        actionData,
-        globalValidate,
-        pubSub,
-      );
-      return fieldResolverMetadata.handler.apply(targetInstance, params);
-    }
-    // getter
-    return targetInstance[fieldResolverMetadata.methodName];
+    return applyMiddlewares(fieldResolverMetadata, actionData, authChecker, async () => {
+      // method
+      if (fieldResolverMetadata.handler) {
+        const params: any[] = await getParams(
+          fieldResolverMetadata.params!,
+          actionData,
+          globalValidate,
+          pubSub,
+        );
+        return fieldResolverMetadata.handler.apply(targetInstance, params);
+      }
+      // getter
+      return targetInstance[fieldResolverMetadata.methodName];
+    });
   };
 }
 
@@ -65,7 +67,26 @@ export function createSimpleFieldResolver(
   const authChecker = BuildContext.authChecker;
   return async (root, args, context, info) => {
     const actionData: ActionData = { root, args, context, info };
-    await checkForAccess(actionData, authChecker, fieldMetadata.roles);
-    return root[fieldMetadata.name];
+    // TODO: handle simple field middlewares
+    // return await applyMiddlewares(fieldMetadata, actionData, authChecker, () => {
+      return root[fieldMetadata.name];
+    // });
   };
+}
+
+async function applyMiddlewares(
+  resolverMetadata: BaseResolverMetadata,
+  actionData: ActionData,
+  authChecker: AuthChecker<any> | undefined,
+  resolverHandlerFunction: () => any,
+) {
+  await checkForAccess(actionData, authChecker, resolverMetadata.roles);
+  for (const middleware of resolverMetadata.beforeMiddlewares!) {
+    await middleware(actionData);
+  }
+  const result = await resolverHandlerFunction();
+  for (const middleware of resolverMetadata.afterMiddlewares!) {
+    await middleware(actionData, result);
+  }
+  return result;
 }
