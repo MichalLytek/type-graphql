582c555eb6ae1af8484f0a0caebc5e6a92d3604f
Dave Wasmer <davewasmer@gmail.com>
2019-01-21 17:58:07 -0500

    add failing test

--- tests/helpers/circular-refs/good/CircularRef2.ts
+++ tests/helpers/circular-refs/good/CircularRef2.ts
@@ -2,8 +2,17 @@ import { Field, ObjectType } from "../../../../src";
 
 import { CircularRef1 } from "./CircularRef1";
 
+let hasModuleFinishedInitialLoad = false;
+
 @ObjectType()
 export class CircularRef2 {
-  @Field(type => CircularRef1)
-  ref1Field: any;
+  @Field(type => {
+    if (!hasModuleFinishedInitialLoad) {
+      throw new Error("Field type function was called synchronously during module load");
+    }
+    return [CircularRef1];
+  })
+  ref1Field: any[];
 }
+
+hasModuleFinishedInitialLoad = true;
