74544dd8aa8f7e58cd0e22ed628aafdf5d6b1a7d
Daniel Huth <daniel.huth1996@gmail.com>
2019-02-03 09:15:43 +0100

    Updated tests and docs to to be more concise

--- tests/units/convertToType.ts
+++ tests/units/convertToType.ts
@@ -1,4 +1,5 @@
 import { convertToType } from "../../src/helpers/types";
+import { Expose } from "class-transformer";
 
 describe("convertToType", () => {
   let globalCounter = 0;
@@ -18,18 +19,24 @@ describe("convertToType", () => {
       test: 4,
     };
 
+    expect(globalCounter).toBe(0);
     const convertedObject = convertToType(SampleTarget, testObject);
     expect(convertedObject).toBeDefined();
+    expect(convertedObject instanceof SampleTarget).toBe(true);
     expect(convertedObject).toHaveProperty("test");
+    expect((convertedObject as SampleTarget).test).toEqual(4);
     expect(globalCounter).toBe(1);
   });
 
   it("should not convert a object to the defined target if it is already instance of that Target", async () => {
     const testObject = new SampleTarget(4);
 
+    expect(globalCounter).toBe(1);
     const convertedObject = convertToType(SampleTarget, testObject);
     expect(convertedObject).toBeDefined();
+    expect(convertedObject instanceof SampleTarget).toBe(true);
     expect(convertedObject).toHaveProperty("test");
+    expect((convertedObject as SampleTarget).test).toEqual(4);
     expect(globalCounter).toBe(1);
   });
 });
