075185bb3744d6b0f78510ead5f83b201a934881
Thomas Klaner <thomas@iwa-tech.com>
2019-03-28 21:29:55 +0100

    fix missing resolvers for interface fields
    and other missing properties

--- src/schema/schema-generator.ts
+++ src/schema/schema-generator.ts
@@ -356,11 +356,25 @@ export abstract class SchemaGenerator {
             fields: () => {
               let fields = interfaceType.fields!.reduce<GraphQLFieldConfigMap<any, any>>(
                 (fieldsMap, field) => {
+                  const fieldResolverMetadata = getMetadataStorage().fieldResolvers.find(
+                    resolver =>
+                      resolver.getObjectType!() === interfaceType.target &&
+                      resolver.methodName === field.name &&
+                      (resolver.resolverClassMetadata === undefined ||
+                        resolver.resolverClassMetadata.isAbstract === false),
+                  );
                   fieldsMap[field.schemaName] = {
-                    description: field.description,
                     type: this.getGraphQLOutputType(field.name, field.getType(), field.typeOptions),
                     args: this.generateHandlerArgs(field.params!),
-                    resolve: createBasicFieldResolver(field),
+                    resolve: fieldResolverMetadata
+                      ? createAdvancedFieldResolver(fieldResolverMetadata)
+                      : createBasicFieldResolver(field),
+                    description: field.description,
+                    deprecationReason: field.deprecationReason,
+                    extensions: {
+                      complexity: field.complexity,
+                      ...field.extensions,
+                    },
                   };
                   return fieldsMap;
                 },
@@ -385,10 +399,7 @@ export abstract class SchemaGenerator {
                   if (!typeTarget) {
                     throw new InterfaceResolveTypeError(interfaceType);
                   }
-                  const objectTypeInfo = implementingObjectTypesInfo.find(
-                    type => type.target === typeTarget,
-                  )!;
-                  return objectTypeInfo.type;
+                  return this.objectTypesInfo.find(type => type.target === typeTarget)!.type;
                 },
           }),
         };
