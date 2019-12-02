ac2bded5154d14886c987fb8709805c040582c4e
Michał Lytek <michal.ly@wp.pl>
2019-12-02 21:28:26 +0100

    feat(complexity): remove not needed `GraphQLFieldConfig` definitions

--- src/graphql.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComplexityEstimator } from "graphql-query-complexity";
-
-declare module "graphql/type/definition" {
-  export interface GraphQLFieldConfig<TSource, TContext, TArgs = { [argName: string]: any }> {
-    complexity?: ComplexityEstimator | number;
-  }
-}
