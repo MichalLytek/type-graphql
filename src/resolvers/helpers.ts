6a08dc13e9c781e001901eee986dd1c573c14cec
Sangmin Yoon <sangmin.yoon@croquis.com>
2019-12-10 22:19:27 +0900

    optimize getParams

--- src/resolvers/helpers.ts
+++ src/resolvers/helpers.ts
@@ -9,55 +9,59 @@ import { Middleware, MiddlewareFn, MiddlewareClass } from "../interfaces/Middlew
 import { IOCContainer } from "../utils/container";
 import { AuthMiddleware } from "../helpers/auth-middleware";
 import { convertArgsToInstance, convertArgToInstance } from "./convert-args";
+import { isPromise } from "../utils/isPromise";
 
-export async function getParams(
+export function getParams(
   params: ParamMetadata[],
   resolverData: ResolverData<any>,
   globalValidate: boolean | ValidatorOptions,
   pubSub: PubSubEngine,
-): Promise<any[]> {
-  return Promise.all(
-    params
-      .sort((a, b) => a.index - b.index)
-      .map(async paramInfo => {
-        switch (paramInfo.kind) {
-          case "args":
-            return await validateArg(
-              convertArgsToInstance(paramInfo, resolverData.args),
-              globalValidate,
-              paramInfo.validate,
-            );
-          case "arg":
-            return await validateArg(
-              convertArgToInstance(paramInfo, resolverData.args),
-              globalValidate,
-              paramInfo.validate,
-            );
-          case "context":
-            if (paramInfo.propertyName) {
-              return resolverData.context[paramInfo.propertyName];
-            }
-            return resolverData.context;
-          case "root":
-            const rootValue = paramInfo.propertyName
-              ? resolverData.root[paramInfo.propertyName]
-              : resolverData.root;
-            if (!paramInfo.getType) {
-              return rootValue;
-            }
-            return convertToType(paramInfo.getType(), rootValue);
-          case "info":
-            return resolverData.info;
-          case "pubSub":
-            if (paramInfo.triggerKey) {
-              return (payload: any) => pubSub.publish(paramInfo.triggerKey!, payload);
-            }
-            return pubSub;
-          case "custom":
-            return await paramInfo.resolver(resolverData);
-        }
-      }),
-  );
+): Promise<any[]> | any[] {
+  const paramValues = params
+    .sort((a, b) => a.index - b.index)
+    .map(paramInfo => {
+      switch (paramInfo.kind) {
+        case "args":
+          return validateArg(
+            convertArgsToInstance(paramInfo, resolverData.args),
+            globalValidate,
+            paramInfo.validate,
+          );
+        case "arg":
+          return validateArg(
+            convertArgToInstance(paramInfo, resolverData.args),
+            globalValidate,
+            paramInfo.validate,
+          );
+        case "context":
+          if (paramInfo.propertyName) {
+            return resolverData.context[paramInfo.propertyName];
+          }
+          return resolverData.context;
+        case "root":
+          const rootValue = paramInfo.propertyName
+            ? resolverData.root[paramInfo.propertyName]
+            : resolverData.root;
+          if (!paramInfo.getType) {
+            return rootValue;
+          }
+          return convertToType(paramInfo.getType(), rootValue);
+        case "info":
+          return resolverData.info;
+        case "pubSub":
+          if (paramInfo.triggerKey) {
+            return (payload: any) => pubSub.publish(paramInfo.triggerKey!, payload);
+          }
+          return pubSub;
+        case "custom":
+          return paramInfo.resolver(resolverData);
+      }
+    });
+  if (paramValues.some(isPromise)) {
+    return Promise.all(paramValues);
+  } else {
+    return paramValues;
+  }
 }
 
 export function applyAuthChecker(
