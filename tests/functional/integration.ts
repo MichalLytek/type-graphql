c3b2314717d4bede6e3745675c701edd459fc633
Michał Lytek <michal.ly@wp.pl>
2019-12-31 14:00:48 +0100

    Add GraphQL schema generation integration test

--- tests/functional/integration.ts
+++ tests/functional/integration.ts
@@ -1,8 +1,10 @@
+import "reflect-metadata";
 import { promises as fs } from "fs";
 import directoryTree from "directory-tree";
 import path from "path";
 import util from "util";
 import childProcess from "child_process";
+import { buildSchema } from "type-graphql";
 
 import generateArtifactsDirPath from "../helpers/artifacts-dir";
 import { stringifyDirectoryTrees } from "../helpers/structure";
@@ -11,19 +13,13 @@ const exec = util.promisify(childProcess.exec);
 
 describe("generator integration", () => {
   let cwdDirPath: string;
+  let schema: string;
 
   beforeEach(async () => {
     cwdDirPath = generateArtifactsDirPath("integration");
     await fs.mkdir(cwdDirPath);
-  });
 
-  afterEach(async () => {
-    await fs.rmdir(cwdDirPath, { recursive: true });
-    await new Promise(r => setTimeout(r, 100));
-  });
-
-  it("should generates TypeGraphQL classes files to output folder by running `prisma2 generate`", async () => {
-    const schema = /* prisma */ `
+    schema = /* prisma */ `
       datasource db {
         provider = "sqlite"
         url      = "file:./dev.db"
@@ -58,9 +54,15 @@ describe("generator integration", () => {
         color    Color
       }
     `;
-
     await fs.writeFile(path.join(cwdDirPath, "schema.prisma"), schema);
+  });
 
+  afterEach(async () => {
+    await fs.rmdir(cwdDirPath, { recursive: true });
+    await new Promise(r => setTimeout(r, 100));
+  });
+
+  it("should generates TypeGraphQL classes files to output folder by running `prisma2 generate`", async () => {
     const { stderr } = await exec("npx prisma2 generate", {
       cwd: cwdDirPath,
     });
@@ -74,5 +76,32 @@ describe("generator integration", () => {
 
     expect(stderr).toHaveLength(0);
     expect(directoryStructureString).toMatchSnapshot("files structure");
+  }, 60000);
+
+  it("should be able to use generate TypeGraphQL classes files to generate GraphQL schema", async () => {
+    const { stderr } = await exec("npx prisma2 generate", {
+      cwd: cwdDirPath,
+    });
+    const {
+      UserCrudResolver,
+      PostCrudResolver,
+      UserRelationsResolver,
+      PostRelationsResolver,
+    } = require(cwdDirPath + "/generated/type-graphql");
+    await buildSchema({
+      resolvers: [
+        UserCrudResolver,
+        PostCrudResolver,
+        UserRelationsResolver,
+        PostRelationsResolver,
+      ],
+      emitSchemaFile: cwdDirPath + "/schema.graphql",
+    });
+    const graphQLSchemaSDL = await fs.readFile(cwdDirPath + "/schema.graphql", {
+      encoding: "utf8",
+    });
+
+    expect(stderr).toHaveLength(0);
+    expect(graphQLSchemaSDL).toMatchSnapshot("graphQLSchemaSDL");
   }, 60000);
 });
