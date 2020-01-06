54c626748fd704eecbf0717d16d0c235aac2457b
Michał Lytek <michal.ly@wp.pl>
2020-01-06 16:45:20 +0100

    Add test case for composing custom resolvers from building blocks

---
+++ tests/functional/custom-resolvers.ts
@@ -0,0 +1,101 @@
+import "reflect-metadata";
+import { promises as fs } from "fs";
+import { buildSchema, Query, Resolver, Args, Ctx } from "type-graphql";
+import { graphql } from "graphql";
+
+import generateArtifactsDirPath from "../helpers/artifacts-dir";
+import { generateCodeFromSchema } from "../helpers/generate-code";
+
+describe("relations resolvers execution", () => {
+  let outputDirPath: string;
+
+  beforeAll(async () => {
+    outputDirPath = generateArtifactsDirPath("relations");
+    await fs.mkdir(outputDirPath);
+    const prismaSchema = /* prisma */ `
+      enum Color {
+        RED
+        GREEN
+        BLUE
+      }
+
+      model Post {
+        uuid     String  @id @default(cuid())
+        content  String
+        color    Color
+      }
+    `;
+    await generateCodeFromSchema(prismaSchema, outputDirPath);
+  });
+
+  afterAll(async () => {
+    await fs.rmdir(outputDirPath, { recursive: true });
+    await new Promise(r => setTimeout(r, 100));
+  });
+
+  it("should be possible to use generated inputs, args and types to build own resolvers", async () => {
+    const { Post, FindManyPostArgs } = require(outputDirPath);
+    @Resolver()
+    class CustomResolver {
+      @Query(_returns => [Post])
+      async customFindManyPost(
+        @Args(_type => FindManyPostArgs) args: any,
+        @Ctx() { photon }: any,
+      ) {
+        return await photon.posts.findMany(args);
+      }
+    }
+    const document = /* graphql */ `
+      query {
+        customFindManyPost(
+          first: 1
+          skip: 1
+          where: {
+            content: { startsWith: "Test" }
+          }
+          orderBy: {
+            color: desc
+          }
+        ) {
+          uuid
+          color
+        }
+      }
+    `;
+    const photonMock = {
+      posts: {
+        findMany: jest.fn().mockResolvedValue([
+          {
+            uuid: "b0c0d78e-4dff-4cdd-ba23-9b417dc684e2",
+            color: "RED",
+          },
+          {
+            uuid: "72c8a188-3d46-45b3-b23f-7d420aa282f1",
+            color: "BLUE",
+          },
+        ]),
+      },
+    };
+
+    const graphQLSchema = await buildSchema({
+      resolvers: [CustomResolver],
+      validate: false,
+      emitSchemaFile: outputDirPath + "/schema.graphql",
+    });
+    const graphQLSchemaSDL = await fs.readFile(
+      outputDirPath + "/schema.graphql",
+      { encoding: "utf8" },
+    );
+
+    const { data, errors } = await graphql(graphQLSchema, document, null, {
+      photon: photonMock,
+    });
+
+    expect(errors).toBeUndefined();
+    expect(data).toMatchSnapshot("custom posts resolver mocked response");
+    expect(photonMock.posts.findMany.mock.calls).toMatchSnapshot(
+      "findManyPost call args",
+    );
+    expect(graphQLSchemaSDL).toMatchSnapshot("graphQLSchemaSDL");
+  });
+});
