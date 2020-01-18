0cf457941756460f4fa49e2623d33702b7809fe5
Michał Lytek <michal.wojciech.lytek@gmail.com>
2020-01-18 19:33:57 +0100

    Update Logger decorator

--- examples/extensions/logger.decorator.ts
+++ examples/extensions/logger.decorator.ts
@@ -5,13 +5,16 @@ interface LogOptions {
   level?: number;
 }
 
-export const Logger = (messageOrOptions: string | LogOptions) =>
-  Extensions({
-    log:
-      typeof messageOrOptions === "string"
-        ? {
-            level: 4,
-            message: messageOrOptions,
-          }
-        : messageOrOptions,
-  });
+export function Logger(messageOrOptions: string | LogOptions) {
+  // parse the parameters of the custom decorator
+  const log: LogOptions =
+    typeof messageOrOptions === "string"
+      ? {
+          level: 4,
+          message: messageOrOptions,
+        }
+      : messageOrOptions;
+
+  // return the `@Extensions` decorator with a prepared property
+  return Extensions({ log });
+}
