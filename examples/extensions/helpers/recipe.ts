d13c3479f3924be031df039b7f9b968a938b324a
Sylvain Boulade <sboulade@gmail.com>
2020-01-21 10:57:34 +0100

    Fix import path

--- examples/extensions/helpers/recipe.ts
+++ examples/extensions/helpers/recipe.ts
@@ -1,6 +1,6 @@
 import { plainToClass } from "class-transformer";
 
-import { Recipe } from "./recipe.type";
+import { Recipe } from "../recipe.type";
 
 export function createRecipe(recipeData: Partial<Recipe>): Recipe {
   return plainToClass(Recipe, recipeData);
