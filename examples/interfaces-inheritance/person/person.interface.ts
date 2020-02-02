801a4538aded6eb72a65829c2b39f8f1517617a4
Michał Lytek <michal.wojciech.lytek@gmail.com>
2020-02-02 10:24:43 +0100

    docs(examples): add workaround for interface example

--- examples/interfaces-inheritance/person/person.interface.ts
+++ examples/interfaces-inheritance/person/person.interface.ts
@@ -2,7 +2,10 @@ import { InterfaceType, Field, Int, ID } from "../../../src";
 
 import { IResource } from "../resource/resource.interface";
 
-@InterfaceType()
+@InterfaceType({
+  // workaround for bug: https://github.com/MichalLytek/type-graphql/issues/373
+  resolveType: value => value.constructor.name,
+})
 export abstract class IPerson implements IResource {
   @Field(type => ID)
   id: string;
