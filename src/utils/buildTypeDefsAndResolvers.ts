7ca82f7807ab1c34b1b3946b14d246c224f9538e
Gabriele <Carassale@users.noreply.github.com>
2021-03-04 00:02:13 +0100

    Update buildTypeDefsAndResolvers.ts
    Adds a method to build TypeDefs and Resolvers in Sync mode.

--- src/utils/buildTypeDefsAndResolvers.ts
+++ src/utils/buildTypeDefsAndResolvers.ts
@@ -1,10 +1,19 @@
 import { printSchema } from "graphql";
 
-import { BuildSchemaOptions, buildSchema } from "./buildSchema";
+import { BuildSchemaOptions, buildSchema, buildSchemaSync } from "./buildSchema";
 import { createResolversMap } from "./createResolversMap";
 
 export async function buildTypeDefsAndResolvers(options: BuildSchemaOptions) {
   const schema = await buildSchema(options);
+  return commonBuild(schema);
+}
+
+export function buildTypeDefsAndResolversSync(options: BuildSchemaOptions) {
+  const schema = buildSchemaSync(options);
+  return commonBuild(schema);
+}
+
+function commonBuild(schema) {
   const typeDefs = printSchema(schema);
   const resolvers = createResolversMap(schema);
   return { typeDefs, resolvers };
