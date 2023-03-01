be78140a8eca7c81031e1b2badfab0d2b3b0fc43
carlocorradini <carlo.ita98@gmail.com>
2023-03-01 15:20:11 +0100

    chore(esm): esm and cjs support

---
+++ scripts/package.json.ts
@@ -0,0 +1,17 @@
+import fs from "node:fs";
+import path from "node:path";
+import tsconfig from "../tsconfig.esm.json";
+
+const PACKAGE_JSON = { type: "module" };
+const PACKAGE_JSON_FILE = path.join(
+  __dirname,
+  "..",
+  tsconfig.compilerOptions.outDir,
+  "package.json",
+);
+
+if (fs.existsSync(PACKAGE_JSON_FILE)) {
+  throw new Error(`ESM module package.json '${PACKAGE_JSON_FILE}' already exists`);
+}
+
+fs.writeFileSync(PACKAGE_JSON_FILE, JSON.stringify(PACKAGE_JSON));
