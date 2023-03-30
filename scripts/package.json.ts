f0bdb1a241fe2167fbea9ce6e5da35d7de5f91d7
carlocorradini <carlo.ita98@gmail.com>
2023-03-30 12:45:45 +0200

    chore(esm): better typings and esm support

--- scripts/package.json.ts
+++ scripts/package.json.ts
@@ -1,17 +1,18 @@
 import fs from "node:fs";
 import path from "node:path";
-import tsconfig from "../tsconfig.esm.json";
+import url from "node:url";
+import tsconfig from "../tsconfig.cjs.json" assert { type: "json" };
 
-const PACKAGE_JSON = { type: "module" };
+const PACKAGE_JSON = { type: "commonjs" };
 const PACKAGE_JSON_FILE = path.join(
-  __dirname,
+  typeof __dirname !== "undefined" ? __dirname : path.dirname(url.fileURLToPath(import.meta.url)),
   "..",
   tsconfig.compilerOptions.outDir,
   "package.json",
 );
 
 if (fs.existsSync(PACKAGE_JSON_FILE)) {
-  throw new Error(`ESM module package.json '${PACKAGE_JSON_FILE}' already exists`);
+  throw new Error(`CJS package.json '${PACKAGE_JSON_FILE}' already exists`);
 }
 
 fs.writeFileSync(PACKAGE_JSON_FILE, JSON.stringify(PACKAGE_JSON), { encoding: "utf8", flag: "w" });
