43f7bbd8c33aad8e5032034450e46e87649d4951
carlocorradini <carlo.ita98@gmail.com>
2023-03-31 15:46:45 +0200

    internal(refactor): better typings in bundle

--- scripts/package.json.ts
+++ scripts/package.json.ts
@@ -5,13 +5,11 @@ import tsconfig from "../tsconfig.esm.json";
 const PACKAGE_JSON = { type: "module" };
 const PACKAGE_JSON_FILE = path.join(
   __dirname,
-  "..",
-  tsconfig.compilerOptions.outDir,
-  "package.json",
+  `../${tsconfig.compilerOptions.outDir}/package.json`,
 );
 
 if (fs.existsSync(PACKAGE_JSON_FILE)) {
-  throw new Error(`ESM module package.json '${PACKAGE_JSON_FILE}' already exists`);
+  throw new Error(`ESM package.json '${PACKAGE_JSON_FILE}' already exists`);
 }
 
 fs.writeFileSync(PACKAGE_JSON_FILE, JSON.stringify(PACKAGE_JSON), { encoding: "utf8", flag: "w" });
