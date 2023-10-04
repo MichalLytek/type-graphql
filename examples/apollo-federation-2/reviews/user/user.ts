bb7d3527844d88a3e3938fb8ab424ff03a4dd9f9
Michał Lytek <michal.wojciech.lytek@gmail.com>
2023-10-04 14:41:20 +0200

    Adjust examples to show federation v2 capabilities

--- examples/apollo-federation-2/reviews/user/user.ts
+++ examples/apollo-federation-2/reviews/user/user.ts
@@ -1,10 +1,8 @@
 import { Directive, Field, ID, ObjectType } from "type-graphql";
 
-@Directive("@extends")
 @Directive(`@key(fields: "id")`)
 @ObjectType()
 export class User {
-  @Directive("@external")
   @Field(_type => ID)
   id!: string;
 
